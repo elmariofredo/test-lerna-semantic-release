4340b0a1c26560d6ead1d5fe9a7cbdb39a5959f7
elmariofredo <mario@vejlupek.cz>
2017-03-15 10:32:57 +0100

    chore(ci): move before_script to after_success
    to prevent errors like fatal: could not set upstream of HEAD to origin/master when it does not point
    to any branch.

--- .ci/after_success.sh
+++ .ci/after_success.sh
@@ -9,8 +9,30 @@ fi
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
 
+  # Git auth
+  git config credential.helper store
+  echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
+  git remote set-url origin https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+  git config user.name ${RELEASE_GH_USERNAME}
+  git config user.email ${RELEASE_GH_EMAIL}
+
+  # Prevent log warning by explicitly setting push strategy
+  git config --global push.default simple
+
+  # Npm auth
+  npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+
+  # Fetch tags
+  git fetch --tags
+
+  # Set upstream branch
+  git branch -u origin/$TRAVIS_BRANCH
+
+
   # Release project packages
   npm run semantic-release
+
+
 
   # Deploy project website to Github Pages
 
