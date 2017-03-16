a87d2c86a42dd01ff7eaea111c09608fafb36e13
elmariofredo <mario@vejlupek.cz>
2017-03-16 09:52:44 +0100

    ci(after_success): add debug messages
    to find where is script killed

--- .ci/after_success.sh
+++ .ci/after_success.sh
@@ -13,58 +13,68 @@ if [[ $TRAVIS_BRANCH == 'master' ]]; then
   # Prepare git and npm for release
   #
 
-  # Checkout explicit branch
-  git fetch
-  git checkout master
+    echo "get branch"
 
-  # Git auth
-  git config --global credential.helper store
-  echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
-  git remote set-url origin https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-  git config --global user.name ${RELEASE_GH_USERNAME}
-  git config --global user.email ${RELEASE_GH_EMAIL}
+    # Checkout explicit branch
+    git fetch
+    git checkout master
 
-  # Prevent log warning by explicitly setting push strategy
-  git config --global push.default simple
+    echo "auth"
 
-  # Npm auth
-  npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+    # Git auth
+    git config --global credential.helper store
+    echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
+    git remote set-url origin https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+    git config --global user.name ${RELEASE_GH_USERNAME}
+    git config --global user.email ${RELEASE_GH_EMAIL}
 
-  # Fetch tags
-  git fetch --tags
+    # Prevent log warning by explicitly setting push strategy
+    git config --global push.default simple
 
-  # Set upstream branch
-  git branch -u origin/$TRAVIS_BRANCH
+    echo "npm auth"
+
+    # Npm auth
+    npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+
+    echo "fetch tags"
+
+    # Fetch tags
+    git fetch --tags
+
+    echo "set upstream"
+
+    # Set upstream branch
+    git branch -u origin/$TRAVIS_BRANCH
 
 
   #
   # Build app
   #
 
-  # Run build task
-  yarn build
+    # Run build task
+    yarn build
 
 
   #
   # Release
   #
 
-  # Release project packages
-  yarn semantic-release
+    # Release project packages
+    yarn semantic-release
 
 
   #
   # Deploy project website to Github Pages
   #
 
-  # Build project to tmp/site
-  yarn site
+    # Build project to tmp/site
+    yarn site
 
-  # Init git from sratch in tmp/site and push it to gh-pages branch
-  cd tmp/site
-  git init
-  git add .
-  git commit -m "Deployed to Github Pages"
-  git push --force --quiet master:gh-pages > /dev/null 2>&1
+    # Init git from sratch in tmp/site and push it to gh-pages branch
+    cd tmp/site
+    git init
+    git add .
+    git commit -m "Deployed to Github Pages"
+    git push --force --quiet master:gh-pages > /dev/null 2>&1
 
 fi
